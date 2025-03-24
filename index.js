7f1b9c661bda726e973155339a65f708233f6b75
Dagim Demeke <dagim1077@gmail.com>
2025-03-24 18:54:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const entry = document.getElementById('entry');
+const submit = document.getElementById('submit');
+const banner = document.getElementById('banner');
+let age;
+
+submit.onclick = function () {
+    age = Number(entry.value);
+
+
+    if (age >= 85) {
+        banner.textContent = `Too Old to Drink! 🤷🏾`;
+    }
+    else if (age <= 0) {
+        banner.textContent = `You have to be born to drink buddy! 😭`
+    }
+    else if (age < 21) {
+        banner.textContent = `✋🏽 STOP! You are too yong to drink!`
+    }
+    else {
+        banner.textContent = `Let's Party 🎉🍾`
+    }
+}
