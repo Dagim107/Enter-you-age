84984f1dad796845223eef084705017f0d5579f2
Dagim Demeke <dagim1077@gmail.com>
2025-03-25 12:43:25 -0500

    Spelling fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ submit.onclick = function () {
         banner.textContent = `You have to be born to drink buddy! 😭`
     }
     else if (age < 21) {
-        banner.textContent = `✋🏽 STOP! You are too yong to drink!`
+        banner.textContent = `✋🏽 STOP! You are too young to drink!`
     }
     else {
         banner.textContent = `Let's Party 🎉🍾`
